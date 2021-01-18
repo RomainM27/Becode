5070165cfeb1c34ae0c37b2df1b34467bb28a1b4
RomainM27 <romain.mathieu96@gmail.com>
2021-01-18 12:14:44 +0100

    rename the exo

--- 1.5-exo.js
+++ 1.5-exo.js
@@ -4,4 +4,4 @@ let numb1 = new Number(readlineSync.question('Number with deci'));
 
 let numb2 = new Number(readlineSync.question('Number with deci'));
 
-console.log(Math.trunc(numb1)* numb2);
+console.log(Math.trunc(numb1)* numb2);-
