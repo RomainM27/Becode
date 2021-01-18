320ab49df7df4fb8245e2b73548b5c4bc4bf747a
RomainM27 <romain.mathieu96@gmail.com>
2021-01-18 14:06:53 +0100

    2.2 exo

---
+++ 02 - Control structures/2.2-exo.js
@@ -0,0 +1,7 @@
+const readlineSync = require("readline-sync");
+
+let min = new Number(readlineSync.question('min  '));
+
+let max = new Number(readlineSync.question('ymax  '));
+
+let current = new Number(readlineSync.question('current '));
