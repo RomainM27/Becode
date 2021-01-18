ee1da76ea7330ec0d81b119a9479642158f390b4
RomainM27 <romain.mathieu96@gmail.com>
2021-01-18 14:13:03 +0100

    ajout exo 2.3µ

--- 02 - Control structures/2.2-exo.js
+++ 02 - Control structures/2.2-exo.js
@@ -5,3 +5,13 @@ let min = new Number(readlineSync.question('min  '));
 let max = new Number(readlineSync.question('ymax  '));
 
 let current = new Number(readlineSync.question('current '));
+
+if (min > max) {
+    console.log('Le min est plus grand que le max ');
+}
+else if ( (current > min) && (max > current) ) {
+    console.log('bg ');
+}
+esle{
+    console.log('pas bien ');
+}
