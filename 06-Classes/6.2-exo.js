36cc159ae3f9d366d52936cc2ea8bbd864167091
RomainM27 <romain.mathieu96@gmail.com>
2021-01-21 09:52:47 +0100

    6.2 exo

---
+++ 06-Classes/6.2-exo.js
@@ -0,0 +1,33 @@
+class Rectangle {
+    constructor (topLeftXPos,topLeftYPos,width,length){
+        this.topLeftXPos=  topLeftXPos;
+        this.topLeftYPos = topLeftYPos;
+        this.width= width;
+        this.length= length;
+    }
+    collides(otherRectangle) {
+        let collisionRightLeft = 
+        /**
+         * la droite du 1 est plus grande que la gauche du 2 ===> droit du 1  =  x + length
+         * et
+         * la droite du 2 est plus grande que la gauche de 1
+         */
+        this.topLeftXPos + this.length  >= otherRectangle.topLeftXPos 
+        && otherRectangle.topLeftXPos + otherRectangle.length >= this.topLeftXPos ;
+
+        /**
+         * le haut du 1 est plus grand que le bas du 2 ===> bas du 2 = y - width
+         * et
+         * le haut du 2 est plus grand que le bas du 1
+         */
+        let collisionTopBottom = 
+        this.topLeftYPos >= otherRectangle.topLeftYPos  - otherRectangle.width
+        && otherRectangle.topLeftYPos >= this.topLeftYPos - this.width;
+        return collisionRightLeft && collisionTopBottom
+    }
+}
+
+let rect1 = new Rectangle(1,2,1,2);
+let rect2 = new Rectangle(3,2,1,3);
+let bool = rect1.collides(rect2);
+console.log(bool);
