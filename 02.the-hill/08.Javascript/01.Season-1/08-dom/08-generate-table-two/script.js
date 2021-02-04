6f2f19d4492a3774ba3a1637cfafe3ea8b9504dd
RomainM27 <romain.mathieu96@gmail.com>
2021-02-04 17:08:53 +0100

    ajout d un exem pour le 8.8

--- 02.the-hill/08.Javascript/01.Season-1/08-dom/08-generate-table-two/script.js
+++ 02.the-hill/08.Javascript/01.Season-1/08-dom/08-generate-table-two/script.js
@@ -22,3 +22,22 @@ function tableGeneratorString(rows, cols) {
     tableString += '</table>';
     return tableString;
 }
+
+/* 
+let table = document.createElement("table");
+    table.setAttribute("id", "myTable");
+    document.getElementById("target").appendChild(table);
+
+    for (let i = 0; i < 10; i++) {
+        let row = document.createElement("tr");
+        document.getElementById("myTable").appendChild(row);
+
+        for (let j = 0; j < 10; j++) {
+            let cols = document.createElement("td");
+            let text = document.createTextNode((i + 1) * (j + 1));
+            cols.appendChild(text);
+            row.appendChild(cols);
+        }
+    }
+
+*/
