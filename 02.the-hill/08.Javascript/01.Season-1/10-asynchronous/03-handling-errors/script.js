131add08a206774b9821bdcce962c231dc26e2df
RomainM27 <romain.mathieu96@gmail.com>
2021-02-05 22:11:39 +0100

    try

--- 02.the-hill/08.Javascript/01.Season-1/10-asynchronous/03-handling-errors/script.js
+++ 02.the-hill/08.Javascript/01.Season-1/10-asynchronous/03-handling-errors/script.js
@@ -3,4 +3,20 @@
 
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", () => { 
+        window.lib.getPersons((error , pers) => {
+            if (pers){
+                console.log(pers);
+            }else{
+                console.log(error);
+            }
+            // try {
+            //     console.log(pers)
+            // } catch(error)  {
+            //     console.log(error)
+                
+            // }
+                
+        }) 
+    })
 })();
--- 02.the-hill/08.Javascript/01.Season-1/10-asynchronous/04-promise-get-posts/script.js
+++ 02.the-hill/08.Javascript/01.Season-1/10-asynchronous/04-promise-get-posts/script.js
@@ -3,4 +3,5 @@
 
 (() => {
     // your code here
+    
 })();
