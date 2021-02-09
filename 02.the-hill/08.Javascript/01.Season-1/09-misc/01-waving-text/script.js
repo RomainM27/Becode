7b546fc1ec0d414f5a731d6ebc2100f001753482
RomainM27 <romain.mathieu96@gmail.com>
2021-02-09 17:01:59 +0100

    try to do the 9series

--- 02.the-hill/08.Javascript/01.Season-1/09-misc/01-waving-text/script.js
+++ 02.the-hill/08.Javascript/01.Season-1/09-misc/01-waving-text/script.js
@@ -1,23 +1,60 @@
 // 09-misc/01-waving-text/script.js - 9.1: waving text
-
+const target = document.getElementById("target");
+const targetString = target.textContent;
 
 (() => {
 
     // your code here
-    let target = document.getElementById("target");
-    let targetString = target.textContent;
+
+    target.innerHTML = '';
     console.log(targetString);
     console.log(targetString.length);
-    console.log(targetString[5]);
-    let i=0;
-    
-    for (let i = 0; i < targetString.length; i++) {
-        console.log(targetString[i])
-        targetString[i].fontsize(7) ;
-        
-    }
-    
 
-        
+    //createSpan() // create span
+    //waving()
+    let span;
+    let spantext = "";
+    for (let i = 0; i < targetString.length; i++) {
+        //targetString.length\
+        (function(i){
+            setTimeout( () => {
+            span = document.createElement("span");
+            spantext = document.createTextNode(targetString[i]);
+            span.appendChild(spantext);
+            console.log(span)
+            target.appendChild(span);
+            },8000)
+        })(i);
+    }
  
+    
 })();
+
+/*
+function createSpan() {
+    let span;
+    let spantext = "";
+    for (let i = 0; i < targetString.length; i++) {
+        span = document.createElement("span");
+        spantext = document.createTextNode(targetString[i]);
+        span.appendChild(spantext);
+        target.appendChild(span);
+    }
+}
+
+function waving() {
+    let span;
+    let spantext = "";
+    for (let i = 0; i < targetString.length; i++) {
+        //targetString.length\
+        setTimeout( () => {
+        span = document.createElement("span");
+        spantext = document.createTextNode(targetString[i]);
+        span.appendChild(spantext);
+        console.log(span)
+        target.appendChild(span);
+        },8000)
+    }
+ 
+}
+*/
--- 02.the-hill/08.Javascript/01.Season-1/09-misc/02-typewriter-effect/script.js
+++ 02.the-hill/08.Javascript/01.Season-1/09-misc/02-typewriter-effect/script.js
@@ -4,5 +4,16 @@
 (() => {
 
     // your code here
-
+    let target = document.getElementById("target");
+    let targetText = target.innerHTML;
+    target.innerHTML = '';
+    console.log(targetText)
+    let id = setInterval(() => {
+        for (let i = 0; i < targetText.length; i++) {
+            console.log(targetText[i])
+            if (i === 50){
+                clearInterval(id)
+            }
+        }
+    }, 1000);
 })();
