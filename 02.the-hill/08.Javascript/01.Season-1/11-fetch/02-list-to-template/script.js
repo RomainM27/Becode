31bc2f335ead487f04ecc7a7aeb6ea0fc2b52076
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-02-12 11:13:40 +0100

    Update script.js

--- 02.the-hill/08.Javascript/01.Season-1/11-fetch/02-list-to-template/script.js
+++ 02.the-hill/08.Javascript/01.Season-1/11-fetch/02-list-to-template/script.js
@@ -19,3 +19,30 @@ const content = template.content.querySelector("li");
         }
     })
 })();
+/*
+    document.getElementById("run").addEventListener("click", () => {
+        fetch("http://localhost:3000/heroes")
+            .then(response => response.json())
+            .then(response => {
+                let xmenList = response;
+                for (let i = 0; i < xmenList.length; i++) {
+                    // Utilise la balise template pour reproduire une structure html pour chaque héros.
+                    // <template id="tpl-hero">
+                    let template = document.getElementById("tpl-hero");
+                    // https://developer.mozilla.org/fr/docs/Web/API/Node/cloneNode
+                    let cloneTemplate = template.content.cloneNode(true);
+                    // affiche dans la balise dont l'id est "target"
+                    let target = document.getElementById("target");
+                    target.appendChild(cloneTemplate);
+                    // target.children[i] renvoie à <li class="hero">
+                    // target.children[i].children[0] renvoie à <h4 class="title">
+                    // target.children[i].children[0].children[0] renvoie à <strong class="name">
+                    target.children[i].children[0].children[0].innerHTML = xmenList[i].name;
+                    // target.children[i].children[0].children[1] renvoie à <em class="alter-ego">
+                    target.children[i].children[0].children[1].innerHTML = xmenList[i].alterEgo;
+                    // target.children[i].children[1] renvoie à <p class="powers">
+                    target.children[i].children[1].innerHTML = xmenList[i].abilities;
+                }
+            });
+    })
+*/
